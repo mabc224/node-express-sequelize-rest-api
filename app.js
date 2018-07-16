0d8b36bfbd2aae2ad22f650d8423cc649cb7f7e5
mabc224 <mabc224@gmail.com>
2018-07-16 16:07:59 +0500

    add eslint and fix errors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
 
-var v1Router = require('./routes/index');
+const v1Router = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // *** cross domain requests *** //
 
@@ -23,12 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', v1Router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
